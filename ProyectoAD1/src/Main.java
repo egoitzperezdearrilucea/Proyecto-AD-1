a4e803d4a7c96b6e1a4f7f69e6c3613c832e182f
Egoitz <egoitz.perezdearrilucea@ikasle.egibide.org>
2022-10-14 20:55:34 +0200

    Opciones jugador
    Opciones de creacion y importacion de jugador

--- ProyectoAD1/src/Main.java
+++ ProyectoAD1/src/Main.java
@@ -12,28 +12,114 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+
         //Variables
         Partida partida;
-        Jugador jugador;
+        Jugador jugador = null;
         Enemigo[] enemigos = new Enemigo[10];
+        String opcion;
+        Boolean correcto = false;
+
+
+        //Opciones jugador
+        correcto = false;
+        while (!correcto){
+            System.out.println("");
+            System.out.println("Jugador:");
+            System.out.println("1-Crear nuevo");
+            System.out.println("2-Cargar .dat");
+            opcion = br.readLine();
+
+            switch (opcion){
+                case ("1"):{
+                    jugador = crearJugador();
+                    correcto = true;
+                }break;
+
+                case ("2"):{
+                    jugador = importarJugadorDAT();
+                    correcto = true;
+                }break;
+
+                default: {
+                    System.out.println("no se entiende el input");
+                    correcto = false;
+                }
+
+            }
+        }
+
+        System.out.println("Jugador cargado: " + jugador.getNombre());
 
 
 
     }
 
-    //Importar datos
+    //Funciones
+    public static Jugador crearJugador() throws IOException {
+        String nombre;
+        String opcion = null;
+        Boolean correcto = false;
 
+        System.out.println("Nombre jugador:");
+        nombre = br.readLine();
+
+        System.out.println("Vida = 100");
+        System.out.println("Ataque = 25");
+
+        Jugador jugador = new Jugador(nombre, 100, 25, 0);
+
+
+        while (!correcto) {
+            System.out.println("");
+            System.out.println("Desea exportar el jugador?");
+            System.out.println("1-No");
+            System.out.println("2-Si");
+            opcion=br.readLine();
+
+            switch (opcion) {
+                case ("1"): {
+
+                    correcto = true;
+                }
+                break;
+
+                case ("2"): {
+                    exportarJugadorDAT(jugador);
+                    exportarJugadorXML(jugador);
+                    correcto = true;
+                }
+                break;
+
+                default: {
+                    System.out.println("no se entiende el input");
+                    correcto = false;
+                }
+            }
+        }
+
+        return jugador;
+
+    }
+
+
+
+
+    //Importar datos
     public static Jugador importarJugadorDAT() throws IOException, ClassNotFoundException {
 
         File fichero = new File ("./src/Jugador.dat");
 
         FileInputStream filein = new FileInputStream(fichero);
-
         ObjectInputStream dataIS = new ObjectInputStream(filein);
 
         Jugador  jugador = (Jugador) dataIS.readObject();
 
+        System.out.println("");
         System.out.println("Datos cargados de el jugador:");
         System.out.println("Nombre:" + jugador.getNombre());
         System.out.println("Nivel:" + jugador.getNivel());
@@ -93,7 +179,7 @@ public class Main {
             //cambiar de nombre a las etiquetas XML
             xstream.alias("jugador", Jugador.class);
             //Insrtar los objetos en el XML
-            xstream.toXML(jugador, new FileOutputStream("Jugador.xml"));
+            xstream.toXML(jugador, new FileOutputStream("./src/Jugador.xml"));
         }catch (Exception e)
         {e.printStackTrace();}
 
